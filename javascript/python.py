f2c9f80c68f4f513415528e9f2cecef318dc2596
ngockhue1620 <ngockhuentca2k@gmail.com>
2021-08-20 22:03:25 +0700

    render list question

---
+++ javascript/python.py
@@ -0,0 +1,17 @@
+
+#
+a = "a"
+last_name = "b"
+firs_name = "c"
+print(a,last_name,firs_name)
+
+
+# + - * / ** % ++ --
+a =5 
+a -=1
+b=5
+b +=1
+print(5**2)
+print("%", 3%5)
+print("--",a )
+print("++",b )
